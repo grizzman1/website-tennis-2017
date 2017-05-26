f9b1188fb276b2b8ba341cfa043fb466bfc412f8
plan_matt <plan_matt@live.com>
2017-05-25 23:30:57 -0400

    Some fun with Website Tennis.

---
+++ js/site.js
@@ -0,0 +1,8 @@
+var answer = prompt('Who invented the microprocessor?');
+if (answer === 'Marcian Hoff' ) {
+alert("Correct! Good Job!");
+}
+else{
+alert("Incorrect")
+}
+// Source: https://en.wikipedia.org/wiki/Marcian_Hoff //
